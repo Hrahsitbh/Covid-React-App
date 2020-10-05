4f6d167a578c6ffd372578e4d3a4bd290ee47008
Harshit <harshit@Admins-MacBook-Pro.local>
2020-10-05 16:00:25 +0530

    Updates

---
+++ precache-manifest.8ab681500b9a56ca4403431c931c2ae9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bd10e6216e31a021e79a5733af6c7b7",
+    "url": "/Covid-React-App/index.html"
+  },
+  {
+    "revision": "9acec9b820a27a10513a",
+    "url": "/Covid-React-App/static/css/main.1c68f7a7.chunk.css"
+  },
+  {
+    "revision": "bf02b13379f05bdc7aa8",
+    "url": "/Covid-React-App/static/js/2.585ee782.chunk.js"
+  },
+  {
+    "revision": "c46ade285a7bd9e73d4de71c9509bd70",
+    "url": "/Covid-React-App/static/js/2.585ee782.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9acec9b820a27a10513a",
+    "url": "/Covid-React-App/static/js/main.bfe1ebd1.chunk.js"
+  },
+  {
+    "revision": "4c82b2fd4c1b118d4245",
+    "url": "/Covid-React-App/static/js/runtime-main.790c5363.js"
+  }
+]);
